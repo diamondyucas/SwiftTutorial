ea4c5e533d01bd4aaaf9595a5ed1a6824fdbfd0e
diamond <diamondyucas@gmail.com>
2023-01-26 06:29:22 -0600

    Initial commit

---
+++ Landscape/Landscape/LandscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandscapeApp.swift
+//  Landscape
+//
+//  Created by Dimond Yucas on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct LandscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
